eb8082acaf92261aeb84b665ea4dcaa70427eaa7
Joshua Bradford <jjyet@yahoo.com>
2017-02-15 14:28:41 -0500

    made for loop to display all twelve employees in their own inline block containers

--- src/js/main.js
+++ src/js/main.js
@@ -8,26 +8,25 @@ function getData (callback) {
     });
 }
 
-getData(print);
-
 var info = {};
 
-function print(data, status, xmlstuff){
-    console.log(data.results[0]);
-    console.log(status);
-    console.log(xmlstuff);
+function print(data){
+    console.log(data);
     info = data.results[0];
     $(".container").after(`
-      <div><img src="${info.picture.large}"/></div>
-      <div class="name">${info.name.first} ${info.name.last}</div>
-      <div class="line"></div>
-      <div class="email">${info.email}</div>
-      <div class="address">${info.location.street}</div>
-      <div class="address">${info.location.city} ${info.location.state} ${info.location.postcode}</div>
-      <div class="phone">${info.cell}</div>
-      <div class="ss-num">${info.cell}</div>
+      <div class="individual-container">
+        <div><img src="${info.picture.large}"/></div>
+        <div class="name">${info.name.first} ${info.name.last}</div>
+        <div class="line"></div>
+        <div class="email">${info.email}</div>
+        <div class="address">${info.location.street}</div>
+        <div class="address">${info.location.city} ${info.location.state} ${info.location.postcode}</div>
+        <div class="phone">${info.cell}</div>
+        <div class="ss-num">${info.cell}</div>
+      </div>
     `);
 }
 
-
-$(".container").after(`<div>${info.gender}</div>`);
+for(var count = 0; count < 12; count++){
+    getData(print);
+}

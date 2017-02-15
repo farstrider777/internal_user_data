c1b2b6d5012c35c6ab5faa5ae9bef33a5dc91791
Joshua Bradford <jjyet@yahoo.com>
2017-02-15 14:01:27 -0500

    made function to print html using info shorthand for shorter lines, put employees picture on screen.

--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,15 @@ function getData (callback) {
 
 getData(print);
 
+var info = {};
+
 function print(data, status, xmlstuff){
-    console.log(data)
-    console.log(status)
-    console.log(xmlstuff)
+    console.log(data.results[0]);
+    console.log(status);
+    console.log(xmlstuff);
+    info = data.results[0];
+    $(".container").after(`<div><img src="${info.picture.large}"/></div>`);
 }
+
+
+$(".container").after(`<div>${info.gender}</div>`);
